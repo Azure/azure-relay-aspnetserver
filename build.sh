4e50f12e5bac47ef28a9499ac7c8f909989d8121
ivadim <iva9im@gmail.com>
2019-11-25 11:13:14 -0800

    add linux build scripts

--- build.sh
+++ build.sh
@@ -11,6 +11,21 @@ while [[ -h $source ]]; do
   # symlink file was located
   [[ $source != /* ]] && source="$scriptroot/$source"
 done
-
 scriptroot="$( cd -P "$( dirname "$source" )" && pwd )"
-"$scriptroot/eng/common/build.sh" --build --restore $@
+
+if [ ${TF_BUILD,,}="true" ]; then
+  BuildConfiguration="Release"
+else
+  BuildConfiguration="Debug"
+fi
+
+echo "dotnet build '${scriptroot}/AzureRelayServer.sln' --configuration ${BuildConfiguration}"
+dotnet build "${scriptroot}/AzureRelayServer.sln" --configuration ${BuildConfiguration}
+
+if [ $? -eq 0 ]
+then
+  exit 0
+else
+  echo "Error during running dotnet build" >&2
+  exit 1
+fi
